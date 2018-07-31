3959488c9bcf226f35228ddddb868a482a5264f6
yann <me.yan.xia@qq.com>
2018-07-31 17:21:44 +0800

    Add CrossOrigin

--- src/main/java/io/daocloud/dmp/test/webapp/TestController.java
+++ src/main/java/io/daocloud/dmp/test/webapp/TestController.java
@@ -3,16 +3,14 @@ package io.daocloud.dmp.test.webapp;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/test")
+@CrossOrigin
 public class TestController {
 
     @RequestMapping("/echo")
