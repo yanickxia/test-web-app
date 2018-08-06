795466c635b3d96887c0e89fb6600fce57eaba94
yann <me.yan.xia@qq.com>
2018-08-06 10:39:47 +0800

    Add more test

--- src/main/java/io/daocloud/dmp/test/webapp/TestController.java
+++ src/main/java/io/daocloud/dmp/test/webapp/TestController.java
@@ -1,17 +1,27 @@
 package io.daocloud.dmp.test.webapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
 import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/test")
 @CrossOrigin
 public class TestController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping("/echo")
     public String echo(@RequestBody String input) {
@@ -36,5 +46,28 @@ public class TestController {
         header.setContentLength(documentBody.length);
 
         return new HttpEntity<>(documentBody, header);
+    }
+
+    @RequestMapping("/download")
+    public void downPhotoByStudentId(final HttpServletResponse response,
+                                     @Value("classpath:一个文件.txt") Resource res) throws Exception {
+        String fileName = "一个文件.txt";
+        fileName = URLEncoder.encode(fileName, "UTF-8");
+
+        byte[] data = IOUtils.toByteArray(res.getInputStream());
+
+        response.reset();
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        response.addHeader("Content-Length", "" + data.length);
+        response.setContentType("application/octet-stream;charset=UTF-8");
+        OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+        outputStream.write(data);
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    @RequestMapping("/chinese-param")
+    public String chinesePath(@RequestParam MultiValueMap<String, String> chineseParam) throws Exception {
+        return objectMapper.writeValueAsString(chineseParam);
     }
 }
